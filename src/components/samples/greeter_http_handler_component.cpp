78a80702abb07d021cfd82e07bf3bd85a8e6d894
pavelbezpravel <p.talashchenko@gmail.com>
2023-12-01 14:17:10 +0300

    maintenance: remove unused components

--- src/components/samples/greeter_http_handler_component.cpp
+++
@@ -1,9 +0,0 @@
-#include "greeter_http_handler_component.h"
-
-namespace samples {
-std::string GreeterHttpHandler::HandleRequestThrow(
-    const userver::server::http::HttpRequest& request,
-    userver::server::request::RequestContext&) const {
-  return grpc_greeter_client_.SayHello(request.RequestBody());
-}
-}  // namespace samples
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
-#include "components/samples/greeter_client_component.h"
 #include "components/samples/greeter_service_component.h"
-#include "components/samples/greeter_http_handler_component.h"
+
 
 #include <userver/components/minimal_server_component_list.hpp>
-#include <userver/testsuite/testsuite_support.hpp>
-#include <userver/ugrpc/client/client_factory_component.hpp>
 #include <userver/ugrpc/server/server_component.hpp>
 #include <userver/utils/daemon_run.hpp>
 
 int main(int argc, char* argv[]) {
   const auto component_list =
       userver::components::MinimalServerComponentList()
-          .Append<userver::components::TestsuiteSupport>()
-          .Append<userver::ugrpc::client::ClientFactoryComponent>()
           .Append<userver::ugrpc::server::ServerComponent>()
-          .Append<samples::GreeterClient>()
-          .Append<samples::GreeterServiceComponent>()
-          .Append<samples::GreeterHttpHandler>();
+          .Append<samples::GreeterServiceComponent>();
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
